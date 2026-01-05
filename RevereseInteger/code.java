e645dbacdbd28dc7285537fcd52d53cfef3deccf
Ankita Ghosh <141432077+ankitasure23@users.noreply.github.com>
2026-01-05 12:00:51 +0530

    Document reverse integer approach and complexities
    Added explanation and complexity analysis for reverse integer logic.

--- RevereseInteger/code.java
+++ RevereseInteger/code.java
@@ -14,3 +14,20 @@ class Solution {
         return num;
     }
 }
+
+
+
+Approach:
+Initialize an integer num = 0 to store the reversed number.
+Iterate through the digits of x using a loop:
+Extract the last digit: digit = x % 10.
+Remove the last digit from x: x /= 10.
+Check for overflow before updating num:
+If num > Integer.MAX_VALUE / 10 or (num == Integer.MAX_VALUE / 10 && digit > 7), return 0.
+If num < Integer.MIN_VALUE / 10 or (num == Integer.MIN_VALUE / 10 && digit < -8), return 0.
+If the num is already greater tha than MAX/10 then no matter whatever digit we add at the end it will result in a value greater than MAX/10 and vice versa 
+Update the reversed number: num = num * 10 + digit.
+Return the final reversed number.
+    
+Time Complexity: O(d) — where d is the number of digits in x.
+Space Complexity: O(1) — only constant extra space is used.
