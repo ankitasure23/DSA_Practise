ab57204a15b9f7186c799d365172862541e07d8e
Ankita Ghosh <141432077+ankitasure23@users.noreply.github.com>
2026-01-05 11:54:49 +0530

    Update code.java

--- RevereseInteger/code.java
+++ RevereseInteger/code.java
@@ -1,14 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        long num = 0;
+        int num = 0;
         int sign = 1;
         while(x != 0) {
             int rev = x % 10;
+            if(num > Integer.MAX_VALUE/10 || ((num == Integer.MAX_VALUE) && rev > 7))
+                return 0;
+            else if(num < Integer.MIN_VALUE/10 || ((num == Integer.MIN_VALUE) && rev < -8))
+                return 0;
             num = num*10 + rev;
             x = x/10;
-            if(num < Integer.MIN_VALUE || num > Integer.MAX_VALUE) 
-                return 0; 
         }
-        return (int)num;
+        return num;
     }
 }
