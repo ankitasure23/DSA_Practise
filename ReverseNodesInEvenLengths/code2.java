de1dad79f93591a06d0df8a8f15f6dca56e76791
Ankita Ghosh <141432077+ankitasure23@users.noreply.github.com>
2026-01-09 11:58:09 +0530

    Create code2.java

---
+++ ReverseNodesInEvenLengths/code2.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public static ListNode reverse(ListNode node) {
+        if(node == null || node.next == null) return node;
+        ListNode prev = null;
+        ListNode nextNode = node.next;
+        while(node != null){
+            node.next = prev;
+            prev = node;
+            node = nextNode;
+            if(nextNode != null)
+                nextNode = nextNode.next;
+        }
+        ListNode n = prev;
+        return prev;
+    }
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        ListNode prevNode = null;
+        ListNode nextNode = head.next;
+        ListNode temp = head;
+        ListNode kthNode = temp;
+        int group = 1;
+        int count = 0;
+        while(temp != null) {
+            count = 1;
+            kthNode = temp;
+            for(int i=2;i<=group;i++) {
+                if(kthNode.next!=null) {
+                    count++;
+                    kthNode = kthNode.next;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if(count < group) {
+                if(count % 2 != 0)
+                    break;
+                else {
+                    reverse(temp);
+                    prevNode.next = kthNode;
+                    break;
+                }
+            }
+
+            nextNode = kthNode.next;
+            if(group % 2 != 0) {
+                temp = nextNode;
+                group++;
+                prevNode = kthNode;
+            }
+            else {
+                kthNode.next = null;
+                reverse(temp);
+                temp.next = nextNode;
+                prevNode.next = kthNode;
+                prevNode = temp;
+                temp = nextNode;
+                group++;
+            }
+        }
+        return head;
+    }
+}
