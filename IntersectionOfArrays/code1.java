cac6812bcd6e755904c63c64632f16493f1a5f18
Ankita Ghosh <141432077+ankitasure23@users.noreply.github.com>
2026-02-16 19:42:09 +0530

    Document time and space complexity in intersect method
    Added time and space complexity comments to the intersect method.

---
+++ IntersectionOfArrays/code1.java
@@ -0,0 +1,27 @@
+
+Time complexity : O(n+m) , n = nums1.length; m = nums2.length
+Space complexity : O(S) , S = min(n,m)
+  
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for(int num:nums1) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        
+        for(int num : nums2) {
+            if(map.containsKey(num) && map.get(num) > 0) {
+                map.put(num,map.get(num)-1);
+                result.add(num);
+            }
+        }
+        int[] arr = new int[result.size()];
+        for(int i=0;i<result.size();i++) {
+            arr[i] = result.get(i);
+        }
+        return arr;
+    }
+}
+
