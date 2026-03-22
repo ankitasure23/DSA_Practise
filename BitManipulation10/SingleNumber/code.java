788de3ccf913bf5f8706148af1e7a36603a60a4b
ankitasure23 <bikughoshbst@gmail.com>
2026-03-22 11:44:39 +0530

    Added the single number problem of leetcode

---
+++ BitManipulation10/SingleNumber/code.java
@@ -0,0 +1,11 @@
+package BitManipulation10.SingleNumber;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
